a63e472984bc13cf9f607cafae61b1247fa3f77f
Dakota Smith <dsmith@appareo.com>
2016-10-03 10:00:26 -0700

    Move files around + bring BlueSocket in as source since I've failed at bringing it in via SPM for iOS

---
+++ EventSource/EventSource.h
@@ -0,0 +1,19 @@
+//
+//  EventSource.h
+//  EventSource
+//
+//  Created by Dakota Smith on 10/3/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventSource.
+FOUNDATION_EXPORT double EventSourceVersionNumber;
+
+//! Project version string for EventSource.
+FOUNDATION_EXPORT const unsigned char EventSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSource/PublicHeader.h>
+
+
