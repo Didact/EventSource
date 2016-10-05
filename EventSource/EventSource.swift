7d35571b490ecacbf80001b1effd0a96543750b1
Dakota Smith <dsmith@appareo.com>
2016-10-05 10:44:26 -0700

    Parse and dispatch events

--- EventSource/EventSource.swift
+++ EventSource/EventSource.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Event {
+public struct Event {
     let id: String?
     let event: String?
     let data: String?
@@ -18,7 +18,7 @@ struct Event {
     }
 }
 
-extension _EventSource {
+public extension _EventSource {
     enum State: Int {
         case connecting
         case open
@@ -33,8 +33,8 @@ public class _EventSource {
     var lastID: String?
     var reconnectTime: DispatchTimeInterval
     
-    var onMessage: ((Event) -> Void)?
-    var onError: ((Error) -> Void)?
+    public var onMessage: ((Event) -> Void)?
+    public var onError: ((Error) -> Void)?
     
     private let socket: Socket
     
@@ -59,15 +59,15 @@ public class _EventSource {
         }
         self.readyState = .connecting
         try socket.connect(to: host, port: Int32(port))
-        print(url.path)
         let path: String
         if let q = url.query {
             path = url.path + "?" + q
         } else {
             path = url.path
         }
-        let request = "GET \(path) HTTP/1.0\r\nAccept: text/event-stream\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\n\r\n"
+        let request = "GET \(path) HTTP/1.0\r\nAccept: text/event-stream\r\nCache-Control: no-cache\r\n\r\n"
         try socket.write(from: request)
+        
         
         guard let string = try socket.readString() else {
             throw NSError()
@@ -88,10 +88,10 @@ public class _EventSource {
         }
         
         buffer = string.substring(from: headerEnd.upperBound)
-        DispatchQueue.global(qos: .utility).async {
-            self.runLoop()
-        }
         self.readyState = .open
+      //  DispatchQueue.global(qos: .utility).async {
+            self.runLoop()
+       // }
     }
     
     func close() {
@@ -101,11 +101,17 @@ public class _EventSource {
     func runLoop() {
         while readyState == .open {
             do {
+                do {
                 guard let newData = try socket.readString() else {
                     continue
                 }
-                            
-                buffer.append(newData)
+                    
+                    buffer.append(newData)
+                }
+                catch let e {
+                    print(e)
+                    print("whoops")
+                }
                 
                 let records: ArraySlice<String>
                 let components = buffer.components(separatedBy: "\n\n")
@@ -115,12 +121,50 @@ public class _EventSource {
                     buffer = components.last!
                 }
                 else {
-                    // there will be a last index if we have a suffix of \n\n
-                    records = components[0..<components.indices.last!]
+                    records = components[0..<components.endIndex]
                 }
                 
+                
                 for record in records {
-                    // process
+                    
+                    var id: String?
+                    var event: String?
+                    var data: String?
+                    
+                    let lines = record.components(separatedBy: .newlines).map({$0.trimmingCharacters(in: .whitespacesAndNewlines)}).filter({$0 != ""})
+                    for line in lines {
+                        
+                        if line.hasPrefix(":") {
+                            continue
+                        }
+                        
+                        if line.hasPrefix("id:") {
+                            id = line[line.index(line.startIndex, offsetBy: 3)..<line.endIndex]
+                            self.lastID = record
+                        }
+                        
+                        if line.hasPrefix("event:") {
+                            event = (event ?? "") + line[line.index(line.startIndex, offsetBy: 6)..<line.endIndex].trimmingCharacters(in: .whitespaces)
+                        }
+                        
+                        if line.hasPrefix("data:") {
+                            data = (data ?? "") + line[record.index(line.startIndex, offsetBy: 5)..<line.endIndex].trimmingCharacters(in: .whitespaces)
+                        }
+                        
+                        if line.hasPrefix("retry:") {
+                            guard let i = Int(line[line.index(line.startIndex, offsetBy: 6)..<line.endIndex].trimmingCharacters(in: .whitespaces)) else {
+                                continue
+                            }
+                            self.reconnectTime = .milliseconds(i)
+                        }
+                    }
+                    
+                    let e = Event(id: id, event: event, data: data)
+                    if let m = self.onMessage {
+                        DispatchQueue.global(qos: .utility).async  {
+                            m(e)
+                        }
+                    }
                 }
                 
             }
